31bb0f6486981622732f4bc9f68afcb5b44a6088
Михаил Дорошин <demargorn@gmail.com>
2025-01-02 19:41:32 +0300

    code edited

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,10 @@
 import { useState } from 'react';
 
 const Section = ({ title, description }) => {
-   const [open, setOpen] = useState(true);
-
-   let className = 'section';
-   if (open) {
-      className += ' open';
-   }
+   const [open, setOpen] = useState(false);
 
    return (
-      <section className={className}>
+      <section className={open ? 'section open' : 'section'}>
          <button>toggle</button>
          <h3 className='sectionhead' onClick={() => setOpen(!open)}>
             {title}
